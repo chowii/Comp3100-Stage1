257bec0a7ea1a69ff6743048194dbbc1f019640c
sabbib <schowdhury1@woolworths.com.au>
2021-05-28 11:59:56 +1000

    create compile and run script

---
+++ src/compileAndrun.sh
@@ -0,0 +1,5 @@
+echo 'Compile'
+javac -classpath `pwd`:`pwd`/../lib/activation-1.1.1.jar:`pwd`/../lib/jaxb-api-2.3.1.jar:`pwd`/../lib/org.eclipse.persistence.moxy-2.7.3.jar:`pwd`/../lib/jaxb-api-2.2.jar:`pwd`/../lib/jaxb-impl-2.0.1.jar *.java data/*.java scheduler/*.java
+
+echo 'Run'
+./test_results "java -cp `pwd`:`pwd`/../lib/activation-1.1.1.jar:`pwd`/../lib/jaxb-api-2.3.1.jar:`pwd`/../lib/org.eclipse.persistence.moxy-2.7.3.jar:`pwd`/../lib/jaxb-api-2.2.jar:`pwd`/../lib/jaxb-impl-2.0.1.jar Client" -o ru -c other/ -n
