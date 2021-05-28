93b80812efac35476e87990b890e7553a10d6bbe
sabbib <schowdhury1@woolworths.com.au>
2021-05-28 12:07:25 +1000

    update FirstFitServerProvider, WIP

--- src/scheduler/FirstFitServerProvider.java
+++ src/scheduler/FirstFitServerProvider.java
@@ -11,6 +11,9 @@ public class FirstFitServerProvider implements ServerProvider {
     private ArrayList<Server> mServerArrayList;
     private Job mJob;
 
+    int startingIndexDsServerList = 0;
+    int startingIndexJobServerList = 0;
+
     public FirstFitServerProvider() {
     }
 
@@ -19,35 +22,48 @@ public class FirstFitServerProvider implements ServerProvider {
     }
 
     public void setJob(Job job) {
-        System.out.println("========================================JOB========================================");
-        System.out.println("ID: " + job.getJobId() + " " + job.GET());
+//        System.out.println("========================================JOB========================================");
+//        System.out.println("ID: " + job.getJobId() + " " + job.GET());
         this.mJob = job;
     }
 
     public void setServerArrayList(List<Server> serverArrayList) {
         this.mServerArrayList = (ArrayList<Server>) serverArrayList;
+
     }
 
     @Override
     public String getServer(String serverType, ArrayList<Server> serverList) {
-        for (Server dsServer: mServerArrayList) {
-            for (Server server: serverList) {
+//        considerLogging();
+//        for (int j = startingIndexDsServerList; j < mServerArrayList.size(); j++) {
+//            Server dsServer = mServerArrayList.get(j);
+            for (int i = startingIndexJobServerList; i <serverList.size(); i++) {
+                Server server = serverList.get(i);
                 // && server.getEstimatedRuntime() >= mJob.getEstRuntime()
-                if (dsServer.getType().equals(server.getType())) {
+//                if (dsServer.getType().equals(server.getType())) {
                     if (server.getCoreCount() >= mJob.getCore() && server.getDisk() >= mJob.getDisk() && server.getMemory() >= mJob.getMemory()) {
+//                        startingIndexJobServerList = j;
+                        startingIndexDsServerList = i;
+                        System.out.println("found");
                         return server.getType() + " " + server.getId();
                     }
                 }
-            }
-        }
-        for (Server server : serverList) {
+//            }
+//        }
+        for (Server server : mServerArrayList) {
             if (server.getCoreCount() >= mJob.getCore() && server.getDisk() >= mJob.getDisk() && server.getMemory() >= mJob.getMemory()) {
                 System.out.println("fallback");
-                Server resServer = server;
-                resServer.setId(0);
-                return resServer. getType() + " " + server.getId();
+                server.setId(0);
+                return server.getType() + " " + server.getId();
             }
         }
+        System.out.println("not found");
         return null;
+    }
+
+    private void considerLogging() {
+        System.out.println("=================== ========================");
+        System.out.println("ds: " + startingIndexDsServerList);
+        System.out.println("js: " + startingIndexJobServerList);
     }
 }
